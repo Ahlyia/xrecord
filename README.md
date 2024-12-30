0ff47c2d26334db20fa22d8f64bad0cc2d0bd3a7
Ahlyia Green <ahlyia.email@gmail.com>
2024-12-30 01:10:20 -0800

    v1.0.1 Added Readme, and comments. Hooray!

---
+++ README.md
@@ -0,0 +1,6 @@
+# XRecord
+A wf-recorder wrapper that allows for automatic naming.
+for setup simply compile it yourself, or run the file `xrecord/build/product`
+
+The default save location is `xrecord/outputs/` however you can edit the `preferences.json` file to
+change it to whatever you want.
--- xrecord.cpp
+++ xrecord.cpp
@@ -11,7 +11,7 @@ namespace fs = std::filesystem;
 
 using json = nlohmann::json;
 
-std::string FetchSaveDirectory() {
+std::string FetchSaveDirectory() { // Command to fetch the users save directory. (whether its specified or not.)
     std::ifstream file("./preferences.json");
 
     json data;
@@ -26,7 +26,7 @@ int main() {
     system("clear");
     std::string saveDir = FetchSaveDirectory();
 
-    if (saveDir == "") {
+    if (saveDir == "") { // If they're using default, and it's not found then create it.
         std::cout << "No set saveDirectory, using default.\nTo change this, edit 'preferences.json'\n\n";
         if (fs::is_directory("./output")) {}
         else {
@@ -36,7 +36,7 @@ int main() {
         saveDir = "./output/";
     }   
 
-    int n_files = 1;
+    int n_files = 1; // Start counting files in save folder for automatic naming
 
     for (const auto& entry : fs::directory_iterator(saveDir)) {
         if (fs::is_regular_file(entry)) {
@@ -44,25 +44,25 @@ int main() {
         }
     }
 
-    std::string filename = std::to_string(n_files);
+    std::string filename = std::to_string(n_files); // Make automatic name
     while (filename.length() < 4) {
         filename = "0" + filename;
     }
     filename = filename + "-xrecord.mp4";
     filename = saveDir + filename;
 
-    const std::string command = "wf-recorder --audio --file "+filename;
+    const std::string command = "wf-recorder --audio --file "+filename; // Establish the command used to rec.
 
-    std::cout << "wf-recorder --audio --file "+filename+"\n\n";
+    std::cout << "wf-recorder --audio --file "+filename+"\n\n"; // Print the output location
 
-    FILE* pipe = popen(command.c_str(),"r");
+    FILE* pipe = popen(command.c_str(),"r"); // Run the command and do some freaky fukshit
     if (!pipe) {
         std::cerr << "pOpen failed..\n";
         return 1;
     } 
 
     std::string _;
-    std::cin >> _;
+    std::cin >> _; // Wait for the users input so it doesnt instantly stop recording...
 
     return 0;
 }
